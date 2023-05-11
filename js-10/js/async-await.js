61a5c85d9901fc4fa3736ae36bf2f8f79813e5b2
Jean Paul Ferreira <jeanpaul@jepafe.net>
2023-05-11 16:32:37 -0300

    21. Workshop Javascript

---
+++ js-10/js/async-await.js
@@ -0,0 +1,32 @@
+const multiplicar = (num1, num2) => {
+  if (typeof num1 != "number" || typeof num2 != "number") {
+    return Promise.reject("Los valores no son numeros");
+  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = num1 * num2;
+      resolve({ num1, num2, result });
+    }, Math.floor(Math.random() * 2000));
+  });
+};
+
+const tabla = async () => {
+  try {
+    for (let j = 2; j <= 3; j++) {
+      for (let i = 1; i <= 10; i++) {
+        const res = await multiplicar(j, i);
+        console.log(`${res.num1} * ${res.num2} = ${res.result}`);
+      }
+    }
+
+    // res = await multiplicar(2, 2);
+    // console.log(`${res.num1} * ${res.num2} = ${res.result}`);
+
+    // res = await multiplicar(2, 3);
+    // console.log(`${res.num1} * ${res.num2} = ${res.result}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+tabla();
