7c8b0a2d6944b99b04fbef9fb4c01e5d570821ca
Jean Paul Ferreira <jeanpaul@jepafe.net>
2023-06-29 13:24:56 -0300

    35. Multer y Express Validator

---
+++ node-09/src/routes/userRouter.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "public/uploads"),
+  filename: (req, file, cb) => cb(null, Date.now() + "_" + file.originalname),
+});
+
+const uploadFile = multer({ storage });
+
+const controller = require("../controllers/userController");
+
+const { body } = require("express-validator");
+const validateInput = require("../middlewares/validator");
+
+const userValidations = [
+  body("email")
+    .isEmail()
+    .withMessage("Ingrese una dirección de correo electrónico válida"),
+  body("password")
+    .isStrongPassword({
+      minLength: 6,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .withMessage("La contraseña debe tener ..."),    
+];
+
+router.get("/create", controller.create);
+router.post("/", userValidations, validateInput, uploadFile.single('image'), controller.store);
+
+router.get("/", controller.index);
+router.get("/:id", controller.show);
+
+module.exports = router;
